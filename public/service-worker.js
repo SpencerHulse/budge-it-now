fb4863a952842b3cbe272e26bbe7d406a52c14d0
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-10 14:50:38 -0400

    Added service worker functions to deal with caching and retrieving cache data.

---
+++ public/service-worker.js
@@ -0,0 +1,64 @@
+const APP_PREFIX = "BudgeItNow-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+];
+
+// Cache resources by self referring to the service worker object
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    // Add files to the cache
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log(`Installing cache : ${CACHE_NAME}`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Clear the cache
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    // Gets the keys in cache and filters it for this apps keys
+    caches.keys().then(function (keyList) {
+      let cacheKeepList = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+
+      // Push the new files to the cacheKeepList
+      cacheKeepList.push(CACHE_NAME);
+
+      // Promise that only resolves when old version of the cache is deleted
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeepList.indexOf(key) === -1) {
+            console.log(`Deleting cache : ${keyList[i]}`);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Retrieves information from the cache
+self.addEventListener("fetch", function (e) {
+  console.log(`Fetch request : ${e.request.url}`);
+  e.respondWith(
+    // Checks for a match with the request
+    caches.match(e.request).then(function (request) {
+      // Returns the request if already cached or fetches it if not
+      if (request) {
+        console.log(`responding with cache : ${e.request.url}`);
+        return request;
+      } else {
+        console.log(`file is not cached, fetching : ${e.request.url}`);
+        return fetch(e.request);
+      }
+      // return request || fetch(e.request);
+    })
+  );
+});
