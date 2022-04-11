06ecdc789e95f1a6efcf9f3b4d89a039855d2a69
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-11 16:52:02 -0400

    Temporary fix for service worker fetch request.

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,26 +2,28 @@ const APP_PREFIX = "BudgeItNow-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
+  "/",
   "/index.html",
   "/css/styles.css",
   "/js/index.js",
   "/js/idb.js",
 ];
 
-// Cache resources by self referring to the service worker object
-self.addEventListener("install", function (e) {
-  e.waitUntil(
+self.addEventListener("install", function (event) {
+  console.log(`WORKER: install event in progress.`);
+  event.waitUntil(
     // Add files to the cache
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log(`Installing cache : ${CACHE_NAME}`);
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .then(() => console.log(`WORKER: install complete.`))
   );
 });
 
-// Clear outdated caches
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
     // Gets the keys in cache and filters it for this apps keys
     caches.keys().then((keyList) => {
       let cacheKeepList = keyList.filter(function (key) {
@@ -44,15 +46,52 @@ self.addEventListener("activate", function (e) {
   );
 });
 
-// Retrieves information from the cache
-self.addEventListener("fetch", function (e) {
-  console.log(`Fetch request : ${e.request.url}`);
-  e.respondWith(
-    // Checks for a match with the request
-    caches.match(e.request).then((request) => {
-      // console.log(request);
-      // Returns the request if already cached or fetches it if not
-      return request || fetch(e.request);
+self.addEventListener("fetch", function (event) {
+  // Code given by module/video in module but doesn't actually work offline
+  // console.log("fetch request : " + e.request.url);
+  /*   event.respondWith(
+    caches.match(event.request).then(function (request) {
+      return request || fetch(event.request);
+    })
+  ); */
+
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      let networked = fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve);
+      return cached || networked;
+
+      function fetchedFromNetwork(response) {
+        let cacheCopy = response.clone();
+
+        caches
+          .open(CACHE_NAME)
+          .then(function add(cache) {
+            cache.put(event.request, cacheCopy);
+          })
+          .then(function () {
+            console.log(`Fetch response stored in cache`);
+          });
+        return response;
+      }
+
+      function unableToResolve() {
+        return new Response(`<h1>Service Unavailable</h1>`, {
+          status: 503,
+          statusText: "Service Unavailable",
+          headers: new Headers({ "content-Type": "text/plain" }),
+        });
+      }
     })
   );
 });
+
+// Pseudo code given by learning helper
+// set a variable for data cache name --- Don't understand this
+// check to see if event.url includes "/api/" --- Done
+// if it does open then use the data cache name variable to open the cache
+// return fetch event.request
+// if fetch.status is 200/successful
+// update the cache using .clone() method
+// .catch return cache.match (event.request) ------ if the code reaches the catch block it means you are offline and it will serve files from cache
